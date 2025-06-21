5bf19e5b3415cd2b598cedad1e05093d0219ebd8
Benjamín Paredes Ramírez <benjamin.paredesr@outlook.com>
2025-06-21 11:05:08 -0600

    se agregan los productos e imagenes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, signal } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonicModule],
 })
 export class HomePage {
+  productos = signal([{
+    id: 1, nombre: 'Smootie de Fresa', precio: 25, imagen: 'https://picsum.photos/200/300'
+  },{
+    id: 2, nombre: 'Torta de Chocolate', precio: 30, imagen: 'https://picsum.photos/200/301'
+  }, {
+    id: 3, nombre: 'Tostada de Queso', precio: 20, imagen: 'https://picsum.photos/200/302'
+  }])
   constructor() {}
 }
